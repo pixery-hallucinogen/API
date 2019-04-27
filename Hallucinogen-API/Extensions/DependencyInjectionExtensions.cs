684f5137c546318f78e91bf43b992df5a018f612
Ahmet Uysal <auysal16@ku.edu.tr>
2019-04-27 11:27:28 +0300

    Configure JWT Auth

--- Hallucinogen-API/Extensions/DependencyInjectionExtensions.cs
+++ Hallucinogen-API/Extensions/DependencyInjectionExtensions.cs
@@ -63,6 +63,27 @@ namespace Hallucinogen_API.Extensions
             return services;
         }
 
+        public static IServiceCollection AddJwtConfiguration(this IServiceCollection services)
+        {
+            var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]))
+                    };
+                });
+            return services;
+        }
+        
         public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
         {
             var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
